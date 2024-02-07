0f91178b07ac170abc13eb4a286930e8f242f5d4
Yura11 <64985383+Yura11@users.noreply.github.com>
2024-02-07 12:13:11 +0200

    Update yurabel.sh

--- yurabel.sh
+++ yurabel.sh
@@ -7,6 +7,7 @@ cd ki406
 echo '' > log.txt
 echo 'y.belichenko007@gmail.com' > yurabel.txt
 cp yurabel.txt /home
+cp 'Apache_2.4-PHP_5.5-5.6_queriesa (1).log' /home/osboxes/yurabel1/ki406
 
 file_path="/home/osboxes/yurabel1/ki406/yurabel.txt"
 
@@ -21,7 +22,7 @@ else
 fi
 
 log_file="/home/osboxes/yurabel1/ki406/log.txt"
-apache_log="/home/osboxes/yurabel1/ki406/Apache_2.4-PHP_5.5-5.6_queriesa.log"
+apache_log="/home/osboxes/yurabel1/ki406/Apache_2.4-PHP_5.5-5.6_queriesa (1).log"
 
 
 grep ' " 404 ' "$apache_log" | tee "$log_file"
